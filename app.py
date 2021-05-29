4e52135aab7ea7e1d08f06de48782bd416e8affa
Britni Canale <bpc72@cornell.edu>
2021-05-29 00:09:00 -0400

    Added home page

--- app.py
+++ app.py
@@ -4,9 +4,30 @@ app = Flask(__name__)
 app.secret_key = os.urandom(32)               #generates random 32-bit secret_key
 
 
-@app.route("/")                               #root route, redirects to home if logged in, loads login page if not
+@app.route("/", methods = ["GET", "POST"])                               #root route, redirects to home if logged in, loads login page if not
 def home():
     return render_template("home.html")
+
+@app.route("/experience", methods = ["GET", "POST"])
+def experience():
+    return render_template("experience.html")
+
+@app.route("/skills", methods = ["GET", "POST"])
+def skills():
+    return render_template("skills.html")
+
+@app.route("/projects", methods = ["GET", "POST"])
+def projects():
+    return render_template("projects.html")
+
+@app.route("/education", methods = ["GET", "POST"])
+def education():
+    return render_template("education.html")
+
+@app.route("/resume", methods = ["GET", "POST"])
+def resume():
+    return render_template("resume.html")
+
 
 
 if __name__ == "__main__":
