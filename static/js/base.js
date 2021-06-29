77e5e24f7be55b1803d1849ea14648a462052b79
Britni Canale <britnicanale@britnis-mini.myfiosgateway.com>
2021-06-29 19:34:42 -0400

    Removed unnecessary javascript

--- static/js/base.js
+++ static/js/base.js
@@ -1,37 +1,18 @@
-function toggleActive(new_tab){
-    var tabs = ["about-me", "resume", "skills", "experience", "education", "projects"]
-    tabs.forEach(tab => {
-        element = document.getElementById('nav-link-' + tab);
-        console.log(element)
-        if (tab == new_tab){
-            element.classList.add('active');
-        }else{
-            element.classList.remove('active');
-        }
-    })
-}
-
 function showAllExperience(){
     elements = document.getElementsByClassName('all');
     for (element of elements){
         element.style.display = 'block';
-        console.log("block")
     }
 }
 
 function showProgrammingExperience(){
     elements = document.getElementsByClassName('all');
     for (element of elements) {
-        console.log(element.classList)
         if (element.classList.contains('programming')) {
             element.style.display = 'block';
-            console.log("block")
-            console.log(element)
         }
         else{
             element.style.display = 'none';
-            console.log("none")
-            console.log(element)
         }
     }
 }
@@ -39,16 +20,11 @@ function showProgrammingExperience(){
 function showGeneralExperience(){
     elements = document.getElementsByClassName('all');
     for (element of elements){
-        console.log(element.classList)
         if (element.classList.contains('general')) {
             element.style.display = 'block';
-            console.log("block")
-            console.log(element)
         }
         else{
             element.style.display = 'none';
-            console.log("none")
-            console.log(element)
         }
     }
 }
--- static/js/skills.js
+++
@@ -1,9 +0,0 @@
-function toggleCollapse(name){
-    var element = document.getElementById('collapse' + name);
-    var currentState = element.style.display;
-    if (currentState == "block"){
-        element.style.display = "none";
-    }else{
-        element.style.display = "block";
-    }
-}
