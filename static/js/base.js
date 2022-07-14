be59032fae0455ff107c7caa4fc3da6e7ad08e71
Britni Canale <britnicanale@Britnis-MacBook-Pro.local>
2022-07-13 23:08:31 -0700

    Update website with more current information

--- static/js/base.js
+++ static/js/base.js
@@ -2,7 +2,6 @@ function toggleActive(new_tab){
     var tabs = ["about-me", "resume", "skills", "experience", "education", "projects"]
     tabs.forEach(tab => {
         element = document.getElementById('nav-link-' + tab);
-        console.log(element)
         if (tab == new_tab){
             element.classList.add('active');
         }else{
@@ -15,23 +14,17 @@ function showAllExperience(){
     elements = document.getElementsByClassName('all');
     for (element of elements){
         element.style.display = 'block';
-        console.log("block")
     }
 }
 
 function showProgrammingExperience(){
     elements = document.getElementsByClassName('all');
     for (element of elements) {
-        console.log(element.classList)
         if (element.classList.contains('programming')) {
             element.style.display = 'block';
-            console.log("block")
-            console.log(element)
         }
         else{
             element.style.display = 'none';
-            console.log("none")
-            console.log(element)
         }
     }
 }
@@ -39,16 +32,11 @@ function showProgrammingExperience(){
 function showGeneralExperience(){
     elements = document.getElementsByClassName('all');
     for (element of elements){
-        console.log(element.classList)
         if (element.classList.contains('general')) {
             element.style.display = 'block';
-            console.log("block")
-            console.log(element)
         }
         else{
             element.style.display = 'none';
-            console.log("none")
-            console.log(element)
         }
     }
 }
