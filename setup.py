2dac55b5bb0c9e72f26bbc436ed724443d41bd30
Britni Canale <britnicanale@gmail.com>
2024-01-14 22:13:39 -0500

    Updating buildpacks

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='britnicanalewebsite',
+   version='1.0',
+   description='A personal website',
+   author='Britni Canale',
+   author_email='britnicanale@gmail.com',
+   packages=['britnicanalewebsite'],
+   install_requires=['flask', 'os'],
+)
